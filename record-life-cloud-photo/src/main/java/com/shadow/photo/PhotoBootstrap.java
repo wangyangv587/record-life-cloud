2f20daee5332b220ff5ee9c3403eff8528d9e6b3
杨旺 <yangwangv5876@163.com>
2020-09-29 18:06:19 +0800

    init

---
+++ record-life-cloud-photo/src/main/java/com/shadow/photo/PhotoBootstrap.java
@@ -0,0 +1,12 @@
+package com.shadow.photo;
+
+/**
+ * 类 描 述：
+ * 创 建 人：Shadow
+ * 创建时间：2020年09月29日 17时51分53秒
+ */
+public class PhotoBootstrap {
+    public static void main(String[] args) {
+
+    }
+}
