cd7c5934b553b3c6014984dea692f3bf64031c49
杨旺 <yangwangv5876@163.com>
2020-10-02 00:20:27 +0800

    添加自定义注解

--- record-life-cloud-photo/src/main/java/com/shadow/photo/controller/PhotoTypeController.java
+++ record-life-cloud-photo/src/main/java/com/shadow/photo/controller/PhotoTypeController.java
@@ -1,5 +1,10 @@
 package com.shadow.photo.controller;
 
+import cn.hutool.json.JSONObject;
+import com.shadow.common.annotation.ShadowLog;
+import com.shadow.common.util.R;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("photo/type")
 public class PhotoTypeController {
 
+    /**
+     * 测试方法
+     * @param jsonObject
+     * @return
+     */
+    @ShadowLog
+    @PostMapping("add")
+    public R add(@RequestBody JSONObject jsonObject){
+
+        //休眠100ms，查看效果
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return R.ok();
+    }
 
 }
